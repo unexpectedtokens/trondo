3e4aa0ad71e1f9b648cf3df941e7f2422a4c89fd
Daniel <dejong543@gmail.com>
2019-09-10 15:10:46 +0200

    Minor tweaks

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -16,6 +16,7 @@ const auth = async (req, res, next) => {
     req.user = user;
     next();
   } catch (e) {
+    console.log(e);
     res.status(401).send({ error: "Please authenticate" });
   }
 };
