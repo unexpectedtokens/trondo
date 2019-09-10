138ca1cb5e8951902db71af05a6ea69a8eef5916
Daniel <dejong543@gmail.com>
2019-09-10 15:54:51 +0200

    a

--- src/routers/user.js
+++ src/routers/user.js
@@ -21,7 +21,7 @@ router.post("/users", async (req, res) => {
 router.post("/users/login", async (req, res) => {
   try {
     const user = await User.findByCredentials(
-      req.body.email,
+      req.body.email.toLowerCase(),
       req.body.password
     );
     const token = await user.genAuthToken();
